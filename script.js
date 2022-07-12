343f9832f77bf1475c3adf34e7593823d0ad39f3
Ackasi <>
2022-07-12 06:42:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+ /* 
+    SCRIPT.JS
+
+    This is where you'll complete the SkillBuilder. Look at the Markdown tab (in the same pane as the console) for detailed README instuctions!
+*/
+
+function calculateCarpet(){
+
+}
+  console.log("click");
+
+
+calculateCarpetTest();
+{  getRoomWidth(roomNumber)
+   getRoomLength(roomNumber)
+ }
+  let room1Width = getRoomWidth(1);
+  let room2Width = getRoomWidth(2);
+  let room1Length = getRoomLength(1);
+  let room2Length = getRoomLength(2);
+  let totalArea =1.1*((room1Width* room1Length)+(room2Width*room2Length));
+
+  
